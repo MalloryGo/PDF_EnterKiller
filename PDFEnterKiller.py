0c560bef9f06dbcf60853e308927c663dff3eb81
Mallory <25074297+MalloryGo@users.noreply.github.com>
2020-06-15 00:37:12 +0800

    v0.2.0
    Simplify the code

--- PDFEnterKiller.py
+++ PDFEnterKiller.py
@@ -1,12 +1,9 @@
 import time
 import pyperclip
-copy_cache = ""
+
 while True:
     time.sleep(2)
-    copyed_text = pyperclip.paste()
-    if copy_cache != copyed_text:
-        copy_cache = copyed_text
-        changed_text = copyed_text.replace("\r","\\r").replace("\n","\\n").replace("-\\r\\n","").replace("\\r\\n"," ")
-        pyperclip.copy(changed_text)
-    else:
-        pass
+    copyedText = pyperclip.paste()
+    if True:
+        changedText = copyedText.replace("\r", "\\r").replace("\n", "\\n").replace("-\\r\\n", "").replace("\\r\\n", " ")
+        pyperclip.copy(changedText)
